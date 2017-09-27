798fc9e1182954a88f7b8038f7f45425529d86bd
Christian Oder <myself5@carbonrom.org>
2017-09-27 12:56:03 +0200

    Initial commit of Mirrorbits Dummyfile creator

---
+++ Makefile
@@ -0,0 +1,46 @@
+SHELL := /bin/bash
+
+# The name of the executable (default is current directory name)
+TARGET := dummycreator
+.DEFAULT_GOAL: $(TARGET)
+
+# These will be provided to the target
+VERSION := 1.0.0
+BUILD := `git rev-parse HEAD`
+
+# Use linker flags to provide version/build settings to the target
+LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
+
+# go source files, ignore vendor directory
+SRC = $(shell find . -type f -name '*.go' -not -path "./vendor/*")
+
+.PHONY: all build clean install uninstall fmt simplify check run
+
+all: check install
+
+$(TARGET): $(SRC)
+	@go build $(LDFLAGS) -o $(TARGET)
+
+build: $(TARGET)
+	@true
+
+clean:
+	@rm -f $(TARGET)
+
+install:
+	@go install $(LDFLAGS)
+
+uninstall: clean
+	@rm -f $$(which ${TARGET})
+
+fmt:
+	@gofmt -l -w $(SRC)
+
+simplify:
+	@gofmt -s -l -w $(SRC)
+
+check:
+	@test -z $(shell gofmt -l $(TARGET).go | tee /dev/stderr) || echo "[WARN] Fix formatting issues with 'make fmt'"
+
+run: install
+	@$(TARGET)
---
+++ dummycreator.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"hash"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+type FileInfo struct {
+	Size    int64
+	ModTime string
+	Sha1    string
+	Sha256  string
+	Md5     string
+}
+
+func main() {
+
+	// You can get individual args with normal indexing.
+	path := os.Args[1]
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		return
+	}
+
+	h, err := HashFile(path)
+	if err != nil {
+		return
+	}
+
+	h.Size = fi.Size()
+	h.ModTime = fi.ModTime().String()
+
+	fileJson, _ := json.Marshal([]FileInfo{h})
+
+	os.MkdirAll(filepath.Join("dummy/", filepath.Dir(path)), os.FileMode(0755))
+	err = ioutil.WriteFile(filepath.Join("dummy/", path), fileJson, 0644)
+	fmt.Println(filepath.Base(path) + " dummy created")
+}
+
+// HashFile generates a human readable hash of the given file path
+func HashFile(path string) (hashes FileInfo, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+
+	var writers []io.Writer
+
+	hsha1 := newHasher(sha1.New(), &hashes.Sha1)
+	defer hsha1.Close()
+	writers = append(writers, hsha1)
+	hsha256 := newHasher(sha256.New(), &hashes.Sha256)
+	defer hsha256.Close()
+	writers = append(writers, hsha256)
+	hmd5 := newHasher(md5.New(), &hashes.Md5)
+	defer hmd5.Close()
+	writers = append(writers, hmd5)
+
+	if len(writers) == 0 {
+		return
+	}
+
+	w := io.MultiWriter(writers...)
+
+	_, err = io.Copy(w, reader)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+type hasher struct {
+	hash.Hash
+	output *string
+}
+
+func newHasher(hash hash.Hash, output *string) hasher {
+	return hasher{
+		Hash:   hash,
+		output: output,
+	}
+}
+
+func (h hasher) Close() error {
+	*h.output = hex.EncodeToString(h.Sum(nil))
+	return nil
+}
